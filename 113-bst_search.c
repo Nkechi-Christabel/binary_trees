2385b8a107bdac52cf729e587c4c4275f50d89af
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-11-29 12:44:23 -0800

    Fixed betty error in 113_bst_search.

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -5,15 +5,16 @@
  *
  * @tree: Pointer to the root node of the BST to search
  * @value: Value to search in the tree
- * Return: Pointer to the node containing a value equals to `value`, or NULL if not found
+ * Return: Pointer to the node containing a value equals to
+ * `value`, or NULL if not found
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-    if (tree == NULL || tree->n == value)
-        return (bst_t *)tree;
+	if (tree == NULL || tree->n == value)
+		return ((bst_t *)tree);
 
-    if (value < tree->n)
-        return bst_search(tree->left, value);
-    else
-        return bst_search(tree->right, value);
+	if (value < tree->n)
+		return (bst_search(tree->left, value));
+	else
+		return (bst_search(tree->right, value));
 }
