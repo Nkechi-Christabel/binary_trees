d3d5300d519612bbd4d0db0404f6b4d5fa4aa3ec
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-11-30 14:25:50 -0800

    Updated the function that goes through a binary tree using level-order traversal.

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -8,15 +8,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+    size_t left_height, right_height;
 
-	if (tree == NULL)
-		return (0);
+    if (tree == NULL)
+        return 0;
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
 
-	return ((left_height > right_height ? left_height : right_height) + 1);
+    return (left_height > right_height ? left_height : right_height) + 1;
 }
 
 /**
@@ -25,24 +25,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
  * @level: Level to print
  * @func: Pointer to a function to call for each node
  */
-void print_given_level(const binary_tree_t *tree, void (*func)(int),
-		size_t level)
+void print_given_level(const binary_tree_t *tree, size_t level, void (*func)(int))
 {
-	if (tree == NULL)
-		return;
+    if (tree == NULL)
+        return;
 
-	if (level == 1)
-		func(tree->n);
-	else if (level > 1)
-	{
-		print_given_level(tree->left, func, level - 1);
-		print_given_level(tree->right, func, level - 1);
-	}
+    if (level == 1)
+        func(tree->n);
+    else if (level > 1)
+    {
+        print_given_level(tree->left, level - 1, func);
+        print_given_level(tree->right, level - 1, func);
+    }
 }
 
 /**
- * binary_tree_levelorder - Goes through a binary tree using level-order
- * traversal
+ * binary_tree_levelorder - Goes through a binary tree using level-order traversal
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node
  *
@@ -50,11 +48,11 @@ void print_given_level(const binary_tree_t *tree, void (*func)(int),
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
-		return;
+    if (tree == NULL || func == NULL)
+        return;
 
-	size_t level, height = binary_tree_height(tree);
+    size_t level, height = binary_tree_height(tree);
 
-	for (level = 1; level <= height + 1; level++)
-		print_given_level(tree, func, level);
+    for (level = 1; level <= height; level++)
+        print_given_level(tree, level, func);
 }
