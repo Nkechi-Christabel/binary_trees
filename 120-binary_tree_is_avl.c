46232c4815df91bd40849c76af7f80ab35956a15
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-11-30 11:44:15 -0800

    Updated the function that removes a node from an AVL tree

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -2,24 +2,27 @@
 #include <limits.h>
 #include <stdint.h>
 
+#include "binary_trees.h"
+
 /**
- * binary_tree_height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree to measure the height.
+ * binary_tree_height - Measures the height of a binary tree from a given node
  *
- * Return: If tree is NULL, your function must return 0, else return height.
+ * @tree: Pointer to the root node of the tree to measure the height
+ * Return: Height of the tree, 0 if tree is NULL or if it's not a perfect tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (tree)
-    {
-        size_t left_height = 0, right_height = 0;
+	size_t left_height, right_height;
 
-        left_height = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-        right_height = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-        return ((left_height > right_height) ? left_height : right_height);
-    }
-    return (0);
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
+
 
 /**
  * is_balanced_helper - Checks if a binary tree is balanced.
@@ -31,24 +34,25 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 ssize_t is_balanced_helper(const binary_tree_t *tree, size_t min, size_t max)
 {
-    ssize_t left_height, right_height, height_diff;
+	ssize_t left_height, right_height, height_diff;
 
-    if (tree)
-    {
-        left_height = is_balanced_helper(tree->left, min, tree->n - 1);
-        right_height = is_balanced_helper(tree->right, tree->n + 1, max);
+	if (tree)
+	{
+		left_height = is_balanced_helper(tree->left, min, tree->n - 1);
+		right_height = is_balanced_helper(tree->right, tree->n + 1, max);
 
-        if (left_height == -1 || right_height == -1)
-            return (-1);
+		if (left_height == -1 || right_height == -1)
+			return (-1);
 
-        height_diff = left_height > right_height ? left_height - right_height : right_height - left_height;
+		height_diff = left_height > right_height ? left_height - right_height :
+			right_height - left_height;
 
-        if (height_diff > 1 || (size_t)tree->n < min || (size_t)tree->n > max)
-            return (-1);
+		if (height_diff > 1 || (size_t)tree->n < min || (size_t)tree->n > max)
+			return (-1);
 
-        return (1 + (left_height > right_height ? left_height : right_height));
-    }
-    return (0);
+		return (1 + (left_height > right_height ? left_height : right_height));
+	}
+	return (0);
 }
 
 /**
@@ -59,8 +63,9 @@ ssize_t is_balanced_helper(const binary_tree_t *tree, size_t min, size_t max)
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    return (is_balanced_helper(tree, 0, SIZE_MAX) != -1);
+	return (is_balanced_helper(tree, 0, SIZE_MAX) != -1);
 }
+
