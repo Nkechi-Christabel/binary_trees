c842a343bff739993ad99b279b0df0611bf75b39
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-11-30 04:26:38 -0800

    Updated the function that rwqemoves a node from a Binary Search Tree

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,5 +1,26 @@
 #include "binary_trees.h"
 
+/**
+ * find_minimum - Finds the node with the minimum value in a BST
+ *
+ * @node: Pointer to the root node of the BST
+ * Return: Pointer to the node with the minimum value
+ */
+bst_t *find_minimum(bst_t *node)
+{
+    while (node->left != NULL)
+        node = node->left;
+
+    return node;
+}
+
+/**
+ * bst_remove - Removes a node from a Binary Search Tree (BST)
+ *
+ * @root: Pointer to the root node of the tree where the node will be removed
+ * @value: Value to remove in the tree
+ * Return: Pointer to the new root node of the tree after removing the desired value
+ */
 bst_t *bst_remove(bst_t *root, int value)
 {
     if (root == NULL)
@@ -14,7 +35,7 @@ bst_t *bst_remove(bst_t *root, int value)
         if (root->left == NULL)
         {
             bst_t *temp = root->right;
-            // Print the tree before freeing the node
+
             binary_tree_print(root);
             free(root);
             return temp;
@@ -22,18 +43,14 @@ bst_t *bst_remove(bst_t *root, int value)
         else if (root->right == NULL)
         {
             bst_t *temp = root->left;
-            // Print the tree before freeing the node
             binary_tree_print(root);
             free(root);
             return temp;
         }
 
-        // Node with two children: Get the in-order successor (smallest
-        // in the right subtree) and copy its value to this node
         bst_t *temp = find_minimum(root->right);
         root->n = temp->n;
 
-        // Delete the in-order successor
         root->right = bst_remove(root->right, temp->n);
     }
 
